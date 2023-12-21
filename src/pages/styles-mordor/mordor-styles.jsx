fe2e3d12dcea1703a4c99efbeb85fa12ed44e997
William Alves <williamcostacardoso94@gmail.com>
2023-12-20 21:17:09 -0300

    feat: test 480px text

--- src/pages/styles-mordor/mordor-styles.jsx
+++ src/pages/styles-mordor/mordor-styles.jsx
@@ -28,48 +28,16 @@ export const TitleMordor = styled.div`
   align-items: center;
   position: absolute;
   height: 100vh;
-  margin-left: 100px;
-  @media (max-width: 1599px) {
-    margin-top: -50px;
-  }
-  @media (max-width: 1349px) {
-    margin-top: -80px;
-    margin-left: 70px;
-  }
-  @media (max-width: 1249px) {
-    margin-top: -100px;
-    margin-left: 70px;
-  }
-  @media (max-width: 1200px) {
-    margin-top: -150px;
-    margin-left: 70px;
-  }
-  @media (max-width: 1150px) {
-    margin-top: -170px;
-    margin-left: 70px;
-  }
-  @media (max-width: 1024px) {
-    margin-top: -190px;
-    margin-left: 50px;
-  }
-  @media (max-width: 950px) {
-    margin-top: -220px;
-    margin-left: 50px;
-  }
-  @media (max-width: 950px) {
-    margin-top: -250px;
-    margin-left: 50px;
-  }
-  @media (max-width: 480px) {
-    margin-top: -300px;
-    margin-left: 30px;
-  }
+  margin-left: 120px;
 
   .title-mordor {
     font-size: 220px;
+    margin-top: 220px;
+    margin-left: ;
     color: #f2ca53;
     @media (max-width: 1599px) {
       font-size: 210px;
+      margin-top: -320px;
     }
     @media (max-width: 1349px) {
       font-size: 200px;
--- src/pages/styles-mordor/sauron-styles.jsx
+++ src/pages/styles-mordor/sauron-styles.jsx
@@ -1,62 +1,89 @@
 import styled from "styled-components";
 
 export const Sauron = styled.section`
-    background-color: #111111;
-    width: 100%;
-    height: auto;
+  background-color: #111111;
+  width: 100%;
+  height: auto;
 `;
 
 export const TitleSauron = styled.div`
-    display: flex;
-    justify-content: center;
-    p{
-        color: #F2CA52;
-        font-size: 100px;
-        margin-top: 50px;
-    }
+  display: flex;
+  justify-content: center;
+  p {
+    color: #f2ca52;
+    font-size: 100px;
+    margin-top: 50px;
+  }
 `;
 
 export const DivImgAndTextSauron = styled.div`
-    display: flex;
-    background-color: #111111;
-    padding: 30px;
-    margin-top: 20px;
+  display: flex;
+  background-color: #111111;
+  padding: 30px;
+  margin-top: 20px;
 `;
 
 export const DivImgSauron = styled.div`
-    flex: 2;
+  flex: 2;
+  border-radius: 4px;
+  img {
+    width: 100%;
+    height: auto;
+    border: 2px solid #f2ca52;
     border-radius: 4px;
-    img{
-        width: 100%;
-        height: auto;
-        border: 2px solid #F2CA52;
-        border-radius: 4px;
-    }
+  }
 `;
 
 export const DivTextSauron = styled.div`
-    flex: 2;
-    font-size: 38px;
-    color: #F2CA52;
-    padding: 40px;
+  flex: 2;
+  font-size: 38px;
+  color: #f2ca52;
+  padding: 40px;
+  p {
+    @media (max-width: 1600px) {
+      font-size: 35px;
+    }
+    @media (max-width: 1475px) {
+      font-size: 32px;
+    }
+    @media (max-width: 1350px) {
+      font-size: 28px;
+    }
+    @media (max-width: 1217px) {
+      font-size: 25px;
+    }
+    @media (max-width: 1125px) {
+      font-size: 22px;
+    }
+    @media (max-width: 1024px) {
+      font-size: 18px;
+    }
+    @media (max-width: 890px) {
+      font-size: 14px;
+    }
+    @media (max-width: 750px) {
+      font-size: 14px;
+    }
+    @media (max-width: 480px) {
+      font-size: 5px;
+    }
+  }
 `;
 
 export const DivThreeImgSauron = styled.div`
-    display: flex;
-    width: 100%;
-    
+  display: flex;
+  width: 100%;
 `;
 
 export const ImageSauron = styled.div`
-    display: flex;
-    background-color: #111111;
-    padding: 20px;
+  display: flex;
+  background-color: #111111;
+  padding: 20px;
+  border-radius: 4px;
+  padding: 30px 30px 50px 30px;
+  img {
+    width: 100%;
+    border: 2px solid #f2ca52;
     border-radius: 4px;
-    padding: 30px 30px 50px 30px;
-    img{
-        width: 100%;
-        border: 2px solid #F2CA52;
-        border-radius: 4px;
-        
-    }
+  }
 `;
