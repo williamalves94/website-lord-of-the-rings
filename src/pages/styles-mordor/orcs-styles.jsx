7cb063edb692d3fa2964fead665f24dfc58e932e
William Alves <williamcostacardoso94@gmail.com>
2024-01-08 17:34:43 -0300

    feat: title orcs responsiveness done

--- src/pages/styles-mordor/orcs-styles.jsx
+++ src/pages/styles-mordor/orcs-styles.jsx
@@ -21,34 +21,80 @@ export const TitleOrcs = styled.div`
   .title-orcs {
     font-size: 220px;
     color: #f2ca53;
+    @media (max-width: 1650px) {
+      font-size: 190px;
+      margin-top: -100px;
+      margin-left: -50px;
+    }
     @media (max-width: 1550px) {
       font-size: 180px;
       margin-top: -100px;
+      margin-left: -50px;
     }
     @media (max-width: 1350px) {
-      font-size: 150px;
+      font-size: 160px;
       margin-top: -150px;
       margin-left: -50px;
     }
-    @media (max-width: 1040px) {
-      font-size: 130px;
+    @media (max-width: 1150px) {
+      font-size: 140px;
       margin-top: -180px;
-      margin-left: -50px;
+      margin-left: -60px;
     }
-    @media (max-width: 850px) {
-      font-size: 110px;
+    @media (max-width: 1000px) {
+      font-size: 120px;
       margin-top: -220px;
       margin-left: -70px;
     }
-    @media (max-width: 769px) {
+    @media (max-width: 850px) {
       font-size: 100px;
       margin-top: -240px;
       margin-left: -70px;
     }
-    @media (max-width: 769px) {
-      font-size: 60px;
+    @media (max-width: 768px) {
+      font-size: 90px;
+      margin-top: -250px;
+      margin-left: -70px;
+    }
+    @media (max-width: 700px) {
+      font-size: 80px;
+      margin-top: -260px;
+      margin-left: -80px;
+    }
+    @media (max-width: 600px) {
+      font-size: 75px;
+      margin-top: -280px;
+      margin-left: -80px;
+    }
+    @media (max-width: 550px) {
+      font-size: 70px;
+      margin-top: -290px;
+      margin-left: -80px;
+    }
+    @media (max-width: 480px) {
+      font-size: 55px;
       margin-top: -300px;
       margin-left: -80px;
+    }
+    @media (max-width: 400px) {
+      font-size: 50px;
+      margin-top: -320px;
+      margin-left: -85px;
+    }
+    @media (max-width: 360px) {
+      font-size: 45px;
+      margin-top: -330px;
+      margin-left: -87px;
+    }
+    @media (max-width: 320px) {
+      font-size: 40px;
+      margin-top: -340px;
+      margin-left: -87px;
+    }
+    @media (max-width: 300px) {
+      font-size: 36px;
+      margin-top: -340px;
+      margin-left: -87px;
     }
   }
 `;
