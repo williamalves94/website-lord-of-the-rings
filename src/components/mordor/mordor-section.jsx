894c48a4ee4cc067f1d1c93a628aaced5ad6b93e
William Alves <williamcostacardoso94@gmail.com>
2024-01-10 16:19:00 -0300

    feat: modal via contextAPI

--- src/components/mordor/mordor-section.jsx
+++ src/components/mordor/mordor-section.jsx
@@ -6,18 +6,30 @@ import { BalrogSection } from "../mordor/balrog-section";
 import { OrcsSection } from "./orcs-section";
 import { FooterMordorSection } from "../mordor/footer-mordor";
 import { MordorEagleSection } from "../mordor/mordor-eagle-section";
+import { useContext } from "react";
+import { AllContext, AllProvider } from "../../context/context";
 
 const MordorSection = () => {
+  const { ModalMap, opneModal, setOpenModal, handleCloseModal } =
+    useContext(AllContext);
+
   return (
     <div>
-      <HeaderMordorSection />
-      <MordorEagleSection />
-      <MordorMainSection />
-      <SauronSection />
-      <NazgulSection />
-      <BalrogSection />
-      <OrcsSection />
-      <FooterMordorSection />
+      <AllProvider>
+        <ModalMap
+          isOpen={opneModal}
+          handleCloseModal={handleCloseModal}
+          onClose={(e) => setOpenModal(false)}
+        />
+        <HeaderMordorSection />
+        <MordorEagleSection />
+        <MordorMainSection />
+        <SauronSection />
+        <NazgulSection />
+        <BalrogSection />
+        <OrcsSection />
+        <FooterMordorSection />
+      </AllProvider>
     </div>
   );
 };
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,15 +1,35 @@
 import { createContext, useState } from "react";
 import { SidebarSection } from "../components/condado/sidebar";
+import { ModalMap } from "../components/condado/modal-map";
 
 export const AllContext = createContext({});
 
 export const AllProvider = ({ children }) => {
   const [sidebar, setSidebar] = useState(false);
-
   const showSidebar = () => setSidebar(!sidebar);
+
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
   return (
     <AllContext.Provider
-      value={{ SidebarSection, sidebar, setSidebar, showSidebar }}
+      value={{
+        SidebarSection,
+        sidebar,
+        setSidebar,
+        showSidebar,
+        ModalMap,
+        openModal,
+        setOpenModal,
+        handleOpenModal,
+        handleCloseModal,
+      }}
     >
       {children}
     </AllContext.Provider>
