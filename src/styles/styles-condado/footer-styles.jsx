bc01f8ba79dd6b393227e6bf2f97e9f356cd17fc
William Alves <williamcostacardoso94@gmail.com>
2024-01-10 22:37:52 -0300

    feat: footer done

--- src/styles/styles-condado/footer-styles.jsx
+++ src/styles/styles-condado/footer-styles.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Footer = styled.footer`
   width: 100%;
   height: 165px;
-  background-color: black;
+  background-color: #111111;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -17,36 +17,38 @@ export const LogoFooter = styled.div`
     height: 150px;
     padding: 0px;
     width: 380px;
-    margin-left: 80px;
+    //margin-left: 80px;
     align-items: center;
     display: flex;
     cursor: pointer;
 
     @media (max-width: 1250px) {
       width: 330px;
-      margin-left: 60px;
+      //margin-left: 60px;
       align-items: center;
       display: flex;
     }
 
     @media (max-width: 1100px) {
       width: 310px;
-      margin-left: 60px;
+      //margin-left: 60px;
       align-items: center;
       display: flex;
     }
 
     @media (max-width: 950px) {
       width: 300px;
-      margin-left: 50px;
+      //margin-left: 50px;
       align-items: center;
       display: flex;
     }
 
     @media (max-width: 480px) {
       width: 250px;
+      height: 100px;
+      justify-content: center;
       align-items: center;
-      margin-left: 20px;
+      margin-top: 25px;
       display: flex;
     }
   }
--- src/styles/styles-mordor/footer-mordor-styles.jsx
+++ src/styles/styles-mordor/footer-mordor-styles.jsx
@@ -3,11 +3,11 @@ import styled from "styled-components";
 export const FooterMordor = styled.footer`
   width: 100%;
   height: 165px;
-  background-color: black;
+  background-color: #111111;
   display: flex;
   align-items: center;
   justify-content: center;
-  border-top: 2px solid #f2ca53;
+  border-top: 3px solid #f2ca53;
 `;
 
 export const LogoFooterMordor = styled.div`
@@ -16,31 +16,39 @@ export const LogoFooterMordor = styled.div`
     width: 400px;
     height: 150px;
     padding: 0px;
+    width: 380px;
+    //margin-left: 80px;
+    align-items: center;
+    display: flex;
+    cursor: pointer;
+
     @media (max-width: 1250px) {
       width: 330px;
-      margin-left: 60px;
+      //margin-left: 60px;
       align-items: center;
       display: flex;
     }
 
     @media (max-width: 1100px) {
       width: 310px;
-      margin-left: 60px;
+      //margin-left: 60px;
       align-items: center;
       display: flex;
     }
 
     @media (max-width: 950px) {
       width: 300px;
-      margin-left: 50px;
+      //margin-left: 50px;
       align-items: center;
       display: flex;
     }
 
     @media (max-width: 480px) {
       width: 250px;
+      height: 100px;
+      justify-content: center;
       align-items: center;
-      margin-left: 20px;
+      margin-top: 25px;
       display: flex;
     }
   }
